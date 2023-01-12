82da84334bb4131c413dd9183335afd89d98a1db
SivaKrishna6745 <krishnasiva6745@gmail.com>
2023-01-12 12:39:49 +0530

    adding initial boilerplate code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Header() {
-    return <header>Header</header>;
+    return (
+        <header>
+            <img src="" alt="little lemon logo" />
+        </header>
+    );
 }
 
 export default Header;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 function Nav() {
-    return <nav>Nav</nav>;
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">HOME</a>
+                </li>
+                <li>
+                    <a href="#">ABOUT</a>
+                </li>
+                <li>
+                    <a href="#">MENU</a>
+                </li>
+                <li>
+                    <a href="#">RESERVATIONS</a>
+                </li>
+                <li>
+                    <a href="#">ORDER ONLINE</a>
+                </li>
+                <li>
+                    <a href="#">LOGIN</a>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Nav;
