bee6ec6952ed13a5f4bcbf70e231c1e4c0f06960
SivaKrishna6745 <krishnasiva6745@gmail.com>
2023-01-21 22:13:59 +0530

    added header, nav, main, footer initial files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+import Nav from "./Nav";
 
 function App() {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <>
+            <Header />
+            <Nav />
+            <Main />
+            <Footer />
+        </>
     );
 }
 
