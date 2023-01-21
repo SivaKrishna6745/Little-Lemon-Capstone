b509cd0015adde33b4e0613971785f2d04b47729
SivaKrishna6745 <krishnasiva6745@gmail.com>
2023-01-21 21:47:47 +0530

    removed all files to start again

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import "./App.css";
-import Nav from "./components/Nav/Nav";
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import BookingPage from "./pages/BookingPage/BookingPage";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MenuPage from "./pages/MenuPage/MenuPage";
-import ReservationsPage from "./pages/ReservationsPage/ReservationsPage";
 
 function App() {
     return (
-        <>
-            <Nav />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="booking" element={<BookingPage />} />
-                <Route path="about" element={<AboutPage />} />
-                <Route path="menu" element={<MenuPage />} />
-                <Route path="reservations" element={<ReservationsPage />} />
-            </Routes>
-        </>
+        <div>
+            <h1>App</h1>
+        </div>
     );
 }
 
--- src/pages/ReservationsPage/ReservationsPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ReservationsPage() {
-    return <div>ReservationsPage</div>;
-}
-
-export default ReservationsPage;
